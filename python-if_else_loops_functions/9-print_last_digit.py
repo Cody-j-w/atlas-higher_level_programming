315c7c4f955ff1e2369db7aa787889a80a19bbca
Cody Walenciak <codywalenciak@gmail.com>
2024-01-03 20:13:07 +0000

    fix abs check

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
 
 def print_last_digit(number):
-    if number < 0:
-        numMod = -(abs(number) % 10)
-    else:
-        numMod = abs(number) % 10
+    numMod = abs(number) % 10
     print("{}".format(numMod), end="")
     return numMod
