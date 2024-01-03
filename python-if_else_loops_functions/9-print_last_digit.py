fdb7b68c11899f063e59717d84edbcc344e3289b
Cody Walenciak <codywalenciak@gmail.com>
2024-01-03 20:11:19 +0000

    added absolute value check

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 
 def print_last_digit(number):
-    numMod = number % 10
+    if number < 0:
+        numMod = -(abs(number) % 10)
+    else:
+        numMod = abs(number) % 10
     print("{}".format(numMod), end="")
     return numMod
