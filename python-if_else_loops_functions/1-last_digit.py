6a0a6a2eecf050bbb379feedcc08d9afc72dab92
Cody Walenciak <codywalenciak@gmail.com>
2024-01-03 17:21:10 +0000

    add abs check

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-num = number % 10
+
+if number < 0:
+    num = -(abs(number) % 10)
+else:
+    num = abs(number) % 10
 output = f"Last digit of {number} is {num}"
 if num > 5:
     output += " and is greater than 5"
