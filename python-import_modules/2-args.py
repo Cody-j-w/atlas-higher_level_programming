be79975c7d24ff1509e0b15e560733193ca7ca75
Cody <codywalenciak@gmail.com>
2024-01-04 09:06:10 -0800

    style fix

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 from sys import argv
 if __name__ == "__main__":
-    arg_count = len(argv) - 1
-    if arg_count == 1:
+    argc = len(argv) - 1
+    if argc == 1:
         arg = "argument"
     else:
         arg = "arguments"
-    print("{0} {1}".format(arg_count, arg), end=":\n" if arg_count != 0 else ".\n")
+    print("{0} {1}".format(argc, arg), end=":\n" if argc != 0 else ".\n")
     for i in range(1, len(argv)):
         print("{0}: {1}".format(i, argv[i]))
