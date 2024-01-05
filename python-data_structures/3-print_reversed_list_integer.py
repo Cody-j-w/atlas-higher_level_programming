adb2b6b4e4438bbead56ac2ed995c3d692fe0f21
Cody <codywalenciak@gmail.com>
2024-01-05 10:06:18 -0800

    add check for nonexistent list

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
 def print_reversed_list_integer(list=[]):
-    for i in range(len(list)-1, -1, -1):
-        print("{:d}".format(list[i]))
+    if list != None:
+        for i in range(len(list)-1, -1, -1):
+            print("{:d}".format(list[i]))
