1a486b2630cfe070786d85c098d805b6ad00c0ae
Cody <codywalenciak@gmail.com>
2024-01-08 11:14:27 -0800

    added check for an integer within a string

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -3,4 +3,11 @@
 def best_score(a_dict):
     if len(a_dict) == 0:
         return None
+    for i in a_dict:
+        if type(a_dict[i]) is str:
+            a_dict[i] = int(a_dict[i])
     return max(a_dict, key=a_dict.get)
+
+my_dict = {'a': "6", 'b': -2, 'c': -1, 'd': -46}
+print(best_score(my_dict))
+
