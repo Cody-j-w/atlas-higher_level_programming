533c16c309aa1448387812ffeb7575d3b8d1d21d
Cody <codywalenciak@gmail.com>
2024-01-08 10:50:45 -0800

    cleared test prints

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -4,7 +4,3 @@ def best_score(a_dict):
     if len(a_dict) == 0:
         return None
     return max(a_dict, key=a_dict.get)
-
-my_dict = {'a': -23, 'b': -2, 'c': -1, 'd': -46}
-print(best_score(my_dict))
-
