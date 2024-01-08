f060811b730b450f7e17db8b06fbddd53eacd5f6
Cody <codywalenciak@gmail.com>
2024-01-08 10:39:48 -0800

    find highest value in dictionary

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,10 +1,4 @@
 #!/usr/bin/python3
 
 def best_score(a_dict):
-    max = -1000000000
-    for i in a_dict:
-        if a_dict[i] > max:
-            max = a_dict[i]
-    if max == -1000000000:
-        return None
-    return max
+    return max(a_dict, key=a_dict.get)
