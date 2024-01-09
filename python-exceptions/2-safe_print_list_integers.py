a2174accff41189d3d363c90665e040bce78b583
Cody <codywalenciak@gmail.com>
2024-01-09 11:38:34 -0800

    move exception for TypeErrors

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -6,9 +6,9 @@ def safe_print_list_integers(my_list=[], x=0):
         try:
             print("{:d}".format(my_list[i]), end="")
             idx += 1
-        except ValueError:
+        except (TypeError, ValueError):
             pass
-        except (TypeError, IndexError):
+        except (IndexError):
             print("")
             return idx
     print("")
