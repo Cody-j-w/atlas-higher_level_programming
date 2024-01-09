25652db35dcade22318e8e275ebb9dc517d606ae
Cody <codywalenciak@gmail.com>
2024-01-09 11:20:45 -0800

    safe print script

---
+++ python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+def safe_print_life_integers(my_list=[], x=0)
+    idx = 0
+    for i in range(x):
+        try:
+            print("{:d}".format(my_list[i]), end="")
+            idx += 1
+        except ValueError:
+            pass
+        except IndexError:
+            print("")
+            return idx
+    print("")
+    return idx
+
