29cb30a9854e80599ceb1ca2fa782fda7923f8ce
Cody <codywalenciak@gmail.com>
2024-01-09 12:13:58 -0800

    removed an unnecessary exception?

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -2,18 +2,11 @@
 
 def safe_print_list_integers(my_list=[], x=0):
     idx = 0
-    try:
-        for i in range(x):
-            try:
-                print("{:d}".format(my_list[i]), end="")
-                idx += 1
-            except (TypeError, ValueError):
-                pass
-            except (IndexError):
-                print("")
-                return idx
-        print("")
-        return idx
-    except TypeError:
-        return idx
-
+    for i in range(x):
+        try:
+            print("{:d}".format(my_list[i]), end="")
+            idx += 1
+        except (TypeError, ValueError):
+            continue
+    print("")
+    return idx
