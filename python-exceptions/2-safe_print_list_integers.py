8c5882a9673968f8c32ecfe808990a1aa7609e7c
Cody <codywalenciak@gmail.com>
2024-01-09 11:59:30 -0800

    trying some additional error catches

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -2,18 +2,18 @@
 
 def safe_print_list_integers(my_list=[], x=0):
     idx = 0
-    if my_list is None:
+    try:
+        for i in range(x):
+            try:
+                print("{:d}".format(my_list[i]), end="")
+                idx += 1
+            except (TypeError, ValueError):
+                pass
+            except (IndexError):
+                print("")
+                return idx
+        print("")
         return idx
-    for i in range(x):
-        try:
-            print("{:d}".format(my_list[i]), end="")
-            idx += 1
-        except (TypeError, ValueError):
-            pass
-        except (IndexError):
-            print("")
-            return idx
-    print("")
-    return idx
-
+    except TypeError:
+        return idx
 
