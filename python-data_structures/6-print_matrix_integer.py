e9c17047dedc0e6cdaec4b9ab1fbebd408641351
Cody <codywalenciak@gmail.com>
2024-01-09 09:01:18 -0800

    add a check for None being passed as a matrix

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 
 def print_matrix_integer(matrix=[[]]):
-    for i in matrix:
-        for j in range(len(i)):
-            temp = i[j]
-            print("{:d}".format(temp), end=" " if j != len(i) - 1 else "\n")
+    if matrix is not None:
+        for i in matrix:
+            for j in range(len(i)):
+                temp = i[j]
+                print("{:d}".format(temp), end=" " if j != len(i) - 1 else "\n")
