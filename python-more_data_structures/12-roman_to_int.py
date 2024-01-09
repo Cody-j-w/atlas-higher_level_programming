384fb26822998c22bee0a2cc735b10d9549a7fd9
Cody <codywalenciak@gmail.com>
2024-01-09 10:12:32 -0800

    add checks for nonexistent strings

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,13 +1,20 @@
 #!/usr/bin/python3
 
-def roman_to_int(str):
+def roman_to_int(s):
     r_v = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     res = 0
-    for i in range(len(str)):
-        if i > 0 and r_v[str[i]] > r_v[str[i - 1]]:
-            res += r_v[str[i]] - 2 * r_v[str[i - 1]]
-            i += 2
-        else:
-            res += r_v[str[i]]
-            i += 1
+    if s is None:
+        return res
+    if type(s) is str:
+        for i in range(len(s)):
+            if i > 0 and r_v[s[i]] > r_v[s[i - 1]]:
+                res += r_v[s[i]] - 2 * r_v[s[i - 1]]
+                i += 2
+            else:
+                res += r_v[s[i]]
+                i += 1
     return res
+
+
+print(roman_to_int(None))
+print(roman_to_int(69))
