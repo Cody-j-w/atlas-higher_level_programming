9b9163a048010aaf8f2b692b8b13538064b12f32
Cody Walenciak <codywalenciak@gmail.com>
2024-01-11 18:00:08 +0000

    Style adjustments

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -4,6 +4,7 @@
 This module contains the definition of a Square class with a size attribute
 """
 
+
 class Square:
     """
     A Square class with a private size attribute
@@ -11,7 +12,7 @@ class Square:
     def __init__(self, size):
         """
         the __init__ method for the Square class
-        
+
         Args:
             size (int): the size of the Square
         """
