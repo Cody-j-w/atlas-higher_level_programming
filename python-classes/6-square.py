0e18839dd30b5dc191abd653f0712f6a505e85bd
Cody Walenciak <codywalenciak@gmail.com>
2024-01-11 21:23:55 +0000

    Add additional validation checks

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -58,6 +58,10 @@ class Square:
         """
         if type(value) is not tuple or len(value) != 2:
             raise TypeError("position must be a tuple of 2 positive integers")
+        for i in value:
+            if i < 0 or type(i) is not int:
+                raise TypeError("position must be a tuple of 2 positive integers")
+
         else:
             self.__position = value
 
