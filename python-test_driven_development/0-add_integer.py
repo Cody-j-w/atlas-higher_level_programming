577a8f8800321c3b314c7e8934640f543a50a0c5
Cody Walenciak <codywalenciak@gmail.com>
2024-01-12 22:19:53 +0000

    Add documentation

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,6 +1,17 @@
 #!/usr/bin/python3
-
+"""
+a basic addition module
+"""
 def add_integer(a, b=98):
+    """
+    a basic addition function
+
+    Args:
+        a: the first integer added
+        b: the second integer added
+    Return:
+        the addition of a and b
+    """
     if type(a) is not int and type(a) is not float:
         raise TypeError("a must be an integer")
     if type(b) is not int and type(b) is not float:
