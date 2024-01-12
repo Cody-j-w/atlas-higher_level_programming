3b9a6eca63af01bef52225711a5412d545843369
Cody Walenciak <codywalenciak@gmail.com>
2024-01-12 20:58:17 +0000

    Remove test print

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -7,8 +7,3 @@ def add_integer(a, b=98):
         raise TypeError("b must be an integer")
 
     return int(a) + int(b)
-
-try:
-    print(add_integer(None, "School"))
-except Exception as e:
-    print(e)
