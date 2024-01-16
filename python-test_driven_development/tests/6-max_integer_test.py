f0376e865b9e9531d0b8b332e4d02d7105df6434
Cody Walenciak <codywalenciak@gmail.com>
2024-01-16 07:58:24 +0000

    Fix typo in module import

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 import unittest
-max_integer = __import__("max_integer").max_integer
+max_integer = __import__("6-max_integer").max_integer
 
 class MaxIntTestCase(unittest.TestCase):
     def test_list_max_end(self):
