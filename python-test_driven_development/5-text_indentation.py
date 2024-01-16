eea5c79572d6aad83917e58544196a088b9d3f80
Cody Walenciak <codywalenciak@gmail.com>
2024-01-16 05:07:58 +0000

    Add space check

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python3
 
 def text_indentation(text):
+    space = False
     if type(text) is not str:
         raise TypeError("text must be a string")
     for i in range(len(text)):
+        if space:
+            space = False
+            continue
         print(text[i], end="")
         if text[i] == "." or text[i] == "?" or text[i] == ":":
             print("\n")
-            if i + 1 < len(text) + 1:
+            if i + 1 < len(text) - 1:
                 if text[i + 1] == " ":
-                    text[i] == "\n"
+                    space = True
