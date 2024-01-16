e2645d2122dfe691f95bae01f8acbe2cd6bd618b
Cody Walenciak <codywalenciak@gmail.com>
2024-01-16 03:10:34 +0000

    Add documentation

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -1,6 +1,17 @@
 #!/usr/bin/python3
 
+"""
+A module containing the print_square function
+"""
+
+
 def print_square(size):
+    """
+    print_square prints a square of "#" characters based on the provided size
+
+    Args:
+        An integer representing the dimensions of the square. Can't be negative
+    """
     if type(size) is not int:
         raise TypeError("size must be an integer")
     if size < 0:
