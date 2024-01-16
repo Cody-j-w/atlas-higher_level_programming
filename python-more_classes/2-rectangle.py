dd54e713d7e989481766e6efda8c61f1b374c61e
Cody Walenciak <codywalenciak@gmail.com>
2024-01-16 18:12:14 +0000

    Adjust area and perimeter methods for a 0 axis

--- python-more_classes/2-rectangle.py
+++ python-more_classes/2-rectangle.py
@@ -46,7 +46,11 @@ class Rectangle:
         self.__height = value
 
     def area(self):
+        if self.width == 0 or self.height == 0:
+            return 0
         return self.width * self.height
 
     def perimeter(self):
+        if self.width == 0 or self.height == 0:
+            return 0
         return (self.width * 2) + (self.height * 2)
