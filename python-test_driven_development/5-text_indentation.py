20b5bee9af77072a07e0760fbda40cfff599487a
Cody Walenciak <codywalenciak@gmail.com>
2024-01-16 05:04:22 +0000

    Fixed loop typo

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -4,8 +4,9 @@ def text_indentation(text):
     if type(text) is not str:
         raise TypeError("text must be a string")
     for i in range(len(text)):
-        print(i, end="")
+        print(text[i], end="")
         if text[i] == "." or text[i] == "?" or text[i] == ":":
-            print("\n")
-            if text[i + 1] == " ":
-                i += 1
+            print("")
+            if i + 1 < len(text) + 1:
+                if text[i + 1] == " ":
+                    text[i] == "\n"
