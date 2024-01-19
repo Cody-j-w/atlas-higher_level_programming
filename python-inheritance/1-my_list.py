6aba43367d2252f5641b9077cbfa1a07f76cca6e
Cody Walenciak <codywalenciak@gmail.com>
2024-01-19 22:13:34 +0000

    MyList list subclass with print_sorted method

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+class MyList(list):
+
+    def print_sorted(self):
+        temp_list = []
+        for i in self:
+            temp_list.append(i)
+        for i in range(len(temp_list)):
+            min = temp_list[i]
+            mindex = i
+            for j in range(i, len(temp_list)):
+                if temp_list[j] < temp_list[mindex]:
+                    min = temp_list[j]
+                    mindex = j
+            if mindex != i:
+                temp_list[mindex] = temp_list[i]
+                temp_list[i] = min
+        print(temp_list)
+
+my_list = MyList([1, 2, 6, 2, 8, 6, 2])
+
+my_list.print_sorted()
