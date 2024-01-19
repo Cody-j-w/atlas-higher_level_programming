55242aaeff10628bc2ddfc42fbff8664a351e49c
Cody Walenciak <codywalenciak@gmail.com>
2024-01-19 20:17:08 +0000

    Add method documentation

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -3,10 +3,14 @@
 """
 Module containing the lookup function, which returns a list of all of the
 methods of the object class passed as a parameter
-
-Args:
-    obj: an object
 """
 
 def lookup(obj):
+    """
+    look up function accepts an object (e.g list, int, string) as a paramater
+    and returns a list of all of the methods that object has
+
+    Args:
+        obj: the object type to retrieve the methods of
+    """
     return dir(obj)
