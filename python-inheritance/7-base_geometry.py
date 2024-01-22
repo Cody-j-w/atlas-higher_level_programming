975b6242517506225904ff85520bfbc810e80a77
Cody Walenciak <codywalenciak@gmail.com>
2024-01-22 22:05:43 +0000

    Remove test print

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -17,6 +17,3 @@ class BaseGeometry:
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
-
-geo = BaseGeometry()
-geo.integer_validator()
