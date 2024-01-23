4ed3232ee3c9b35cfef2e658d423ad47910bea2a
Cody Walenciak <codywalenciak@gmail.com>
2024-01-23 04:09:53 +0000

    Add_item script

---
+++ python-input_output/7-add_item.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+"""
+this module saves and loads entries to and from a JSON file
+"""
+
+save = __import__('5-save_to_json_file').save_to_json_file
+load = __import__('6-load_from_json_file').load_from_json_file
+import sys
+from os.path import isfile
+
+if isfile('./add_item.json'):
+    object_list = load('./add_item.json')
+else:
+    object_list = []
+for i in range(1, len(sys.argv)):
+    object_list.append(sys.argv[i])
+save(object_list, 'add_item.json')
+
