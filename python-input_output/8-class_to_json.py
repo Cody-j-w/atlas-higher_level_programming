1c49df3c6f8e9ab940dd850aaea4a7a50c93ccd4
Cody Walenciak <codywalenciak@gmail.com>
2024-01-23 17:58:32 +0000

    JSON serialization function

---
+++ python-input_output/8-class_to_json.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+"""
+Module containing function to build a dictionary out of the attributes
+of a class, for the purposes of JSON serialization
+"""
+
+
+def class_to_json(obj):
+    """
+    class_to_json takes the attributes of a provided object - obj - and
+    places them into a dictionary format for JSON serialization
+
+    Args:
+        obj: the class object being serialized
+    """
+
+    attrs = [att for att in dir(obj) if not callable(getattr(obj, a))]
+    attr_dict = dict()
+    for att in attrs:
+        attr_dict[att] = obj.att
+    return attr_dict
