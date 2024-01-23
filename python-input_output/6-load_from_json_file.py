2bf2dc0efc38d4d3daa236b0cae50bdcc2191abe
Cody Walenciak <codywalenciak@gmail.com>
2024-01-23 03:45:23 +0000

    Add function to load from a json file

---
+++ python-input_output/6-load_from_json_file.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+"""
+module containing a function that reads the contents of a file
+"""
+
+
+def read_file(filename=""):
+    """
+    read_file opens a file, reads its contents, and prints those contents
+    """
+    
+    with open(filename, 'r') as file:
+        json_obj = json.loads(file)
+    return json_obj
