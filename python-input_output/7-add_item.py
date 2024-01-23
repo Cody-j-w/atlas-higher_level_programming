2548844c8344f7897b91d9a6820e7d02595837b6
Cody Walenciak <codywalenciak@gmail.com>
2024-01-23 04:14:08 +0000

    Further style adjustments, improved documentation

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
+
+"""
+this module saves and loads entries to and from a JSON file.
+New entries are passed as arguments to the script, and saved
+to a list saved as JSON using the save_to_json_file and load_from_json_file
+functions
+"""
 import sys
 from os.path import isfile
-
-"""
-this module saves and loads entries to and from a JSON file
-"""
-
 save = __import__('5-save_to_json_file').save_to_json_file
 load = __import__('6-load_from_json_file').load_from_json_file
 
