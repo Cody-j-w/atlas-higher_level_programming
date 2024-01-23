fc139c399c3307607a8205d45d9f58114eaceea1
Cody Walenciak <codywalenciak@gmail.com>
2024-01-23 00:52:19 +0000

    remove extra newline

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -10,4 +10,4 @@ def read_file(filename=""):
     read_file opens a file, reads its contents, and prints those contents
     """
     with open(filename, 'r') as file:
-        print(file.read())
+        print(file.read(), end="")
