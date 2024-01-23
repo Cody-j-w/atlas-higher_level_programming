811a6ff8c9813f835b9f312b74257e27bd670b95
Cody Walenciak <codywalenciak@gmail.com>
2024-01-23 03:53:55 +0000

    Style fixes

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -10,7 +10,7 @@ def load_from_json_file(filename=""):
     """
     read_file opens a file, reads its contents, and prints those contents
     """
-    
+
     with open(filename, 'r') as file:
         json_obj = json.loads(file.read())
     return json_obj
