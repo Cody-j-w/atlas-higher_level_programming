89cfa24188b8c2c190cfff529f9026721ae95571
Cody Walenciak <codywalenciak@gmail.com>
2024-01-25 19:24:06 +0000

    Initial commit of almost_a_circle

---
+++ python-almost_a_circle/models/base.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+"""
+Module containing the Base class model
+"""
+
+class Base:
+    """
+    Base class model
+
+    Attributes:
+        __nb_objects: a count of instantiated objects, used to determine ids
+        id: the identifying number of the instance
+    """
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = self.__nb_objects
