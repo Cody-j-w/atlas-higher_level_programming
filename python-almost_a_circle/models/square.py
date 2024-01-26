d4e107eb06e98d047d16607db02565576f302239
Cody Walenciak <codywalenciak@gmail.com>
2024-01-26 16:46:17 +0000

    Another super init reformat - id placement causing errors

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -12,7 +12,7 @@ class Square(Rectangle):
     """
 
     def __init__(self, size, x=0, y=0, id=None):
-        super().__init__(id, size, size, x, y)
+        super().__init__(size, size, x, y, id)
 
     def __str__(self):
         i = self.id
