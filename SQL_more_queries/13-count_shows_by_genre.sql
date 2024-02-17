76b7aea7fe493f34675cf51ba8c52d23c878dc32
Cody <codywalenciak@gmail.com>
2024-02-17 13:56:02 -0600

    Check for counts of 0

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -5,4 +5,5 @@ SELECT tv_genres.name, COUNT(tv_show_genres.show_id) AS number_of_shows
     LEFT JOIN tv_show_genres
     ON (tv_show_genres.genre_id = tv_genres.id)
     GROUP BY tv_genres.name
+    HAVING COUNT(tv_show_genres.show_id) > 0
     ORDER BY number_of_shows DESC;
