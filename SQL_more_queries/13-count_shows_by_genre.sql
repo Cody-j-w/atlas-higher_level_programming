5ddaa89ff9d7fbd0fe1150a3283e381f69cdc6b2
Cody <codywalenciak@gmail.com>
2024-02-17 13:51:53 -0600

    List genres

---
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,8 @@
+-- list all tv genres with a count of all shows that have that genre
+
+SELECT tv_genres.name, COUNT(tv_show_genres.show_id) as number_of_shows
+    FROM tv_genres
+    LEFT JOIN tv_show_genres
+    ON (tv_show_genres.genre_id = tv_genres.id)
+    GROUP BY tv_genres.name
+    ORDER BY number_of_shows DESC;
