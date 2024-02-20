71d0a1c95e9bca14b5c61dbbe20c3c3215031c96
Cody Walenciak <codywalenciak@gmail.com>
2024-02-20 19:58:39 +0000

    Add CIty model, add relationship

--- python-object_relational_mapping/model_city.py
+++ python-object_relational_mapping/model_city.py
@@ -3,23 +3,24 @@
 """
 Module containing State SQLAlchemy definition
 """
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
+from model_state import State, Base
 
-Base = declarative_base()
 
-
-class State(Base):
+class City(Base):
     """
-    State SQLAlchemy model
+    City SQLAlchemy model
     Attributes:
         __tablename__: the name of the table
         id: an identifying integer
-        name: the name of the state
+        name: the name of the city
+        state_id: the id of the city's state
     """
-    __tablename__ = 'states'
+    __tablename__ = 'cities'
     id = Column(Integer, primary_key=True, autoincrement=True)
     name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'))
 
-    cities = relationship("City", back_populates="state")
+    state = relationship("State", back_populates="cities")
