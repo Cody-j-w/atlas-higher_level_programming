294834c353e244adaf7a71b44bb6debd76876dcb
Cody Walenciak <codywalenciak@gmail.com>
2024-02-20 17:36:00 +0000

    Add checks for missing args

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -8,9 +8,12 @@ from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 from sys import argv
 
-user = argv[1]
-pwd = argv[2]
-db = argv[3]
+if len(argv) > 1:
+    user = argv[1]
+if len(argv) > 2:
+    pwd = argv[2]
+if len(argv) > 3:
+    db = argv[3]
 
 engine = create_engine('mysql+mysqldb://{0}:{1}@localhost:3306/{2}'.format(user, pwd, db))
 
