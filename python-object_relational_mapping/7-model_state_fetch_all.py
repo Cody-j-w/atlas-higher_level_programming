49faf924da5359e328a6d1eb4ed5d8fb3ab1bb73
Cody Walenciak <codywalenciak@gmail.com>
2024-02-20 17:33:21 +0000

    fetch_all query

---
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+"""
+Module containing a SQL Alchemy query to fetch all states in an arg-provided DB
+"""
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+from sys import argv
+
+user = argv[1]
+pwd = argv[2]
+db = argv[3]
+
+engine = create_engine('mysql+mysqldb://{0}:{1}@localhost:3306/{2}'.format(user, pwd, db))
+
+Session = sessionmaker(bind=engine)
+session = Session()
+
+state_records = session.query(State).all()
+
+for state in state_records:
+    print("{}: {}".format(state.id, state.name))
