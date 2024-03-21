101bd6b7d43f4d703b8ba0cadf75ce3e5de7f9c2
Cody <codywalenciak@gmail.com>
2024-03-21 14:24:39 -0500

    Style fixes

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -2,12 +2,10 @@
 
 const { argv } = require('node:process');
 
-if (argv.length < 3){
+if (argv.length < 3) {
     console.log('No argument');
-}
-else if (argv.length == 3){
+} else if (argv.length === 3) {
     console.log('Argument found');
-}
-else{
+} else {
     console.log('Arguments found');
 }
