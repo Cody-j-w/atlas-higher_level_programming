40a10ec2ee79aebab2dcbbbe17bf63e476d72f92
Cody Walenciak <codywalenciak@gmail.com>
2024-03-22 16:34:08 +0000

    Style fixes

--- javascript-warm_up/10-factorial.js
+++ javascript-warm_up/10-factorial.js
@@ -2,12 +2,12 @@
 
 const { argv } = require('node:process');
 
-function factorial(int) {
-    if (Number(int) === 1 || Number(int) === 0 || isNaN(Number(int))) {
-        return 1;
-    } else {
-    return(int * factorial(int - 1));
-    }
+function factorial (int) {
+  if (Number(int) === 1 || Number(int) === 0 || isNaN(Number(int))) {
+    return 1;
+  } else {
+    return (int * factorial(int - 1));
+  }
 }
 
-console.log(factorial(argv[2]))
+console.log(factorial(argv[2]));
