0fff25529c46f78d528a731ddbcb378b7f07cd9b
Cody Walenciak <codywalenciak@gmail.com>
2024-03-22 16:16:37 +0000

    Style fixes

--- javascript-warm_up/9-add.js
+++ javascript-warm_up/9-add.js
@@ -2,8 +2,7 @@
 
 const { argv } = require('node:process');
 
-arg1 = Number(argv[2]);
-arg2 = Number(argv[3]);
-
+const arg1 = Number(argv[2]);
+const arg2 = Number(argv[3]);
 
 console.log(parseInt(+arg1 + +arg2));
