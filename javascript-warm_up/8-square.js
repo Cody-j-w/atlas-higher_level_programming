26bd21697fc4a206296d859290a6ba6df9eb476e
Cody <codywalenciak@gmail.com>
2024-03-22 10:49:46 -0500

    Missing semicolon

--- javascript-warm_up/8-square.js
+++ javascript-warm_up/8-square.js
@@ -5,7 +5,7 @@ const { argv } = require('node:process');
 const size = argv[2];
 
 if (isNaN(Number(size))) {
-  console.log('Missing size')
+  console.log('Missing size');
 } else {
   for (let i = 0; i < size; i++) {
     let line = '';
