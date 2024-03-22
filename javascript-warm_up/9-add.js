8a9c84b93eef0e1b4dab190ca7ddebe68390b6dd
Cody Walenciak <codywalenciak@gmail.com>
2024-03-22 16:09:03 +0000

    Properly convert arguments to numbers

--- javascript-warm_up/9-add.js
+++ javascript-warm_up/9-add.js
@@ -2,4 +2,8 @@
 
 const { argv } = require('node:process');
 
-console.log(Number(argv[2] + Number(argv[3])));
+arg1 = Number(argv[2]);
+arg2 = Number(argv[3]);
+
+
+console.log(parseInt(+arg1 + +arg2));
